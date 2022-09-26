d1c1fd95616f6004904f9ce468b5867463ea071b
RomaDel1fe <romadelife@gmail.com>
2022-09-27 00:15:55 +0300

    added active page for navigation

--- js/main.js
+++ js/main.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
       $('.menu__burger,.menu').toggleClass('menu_active')
       $('body').toggleClass('lock')
    })
+   $('.menu a').each(function(){
+      let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
+      let link = this.href;
+      if(location == link){
+         $(this).parent().addClass('header_active');
+      }
+   })
 })
+
